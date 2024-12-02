f1ba6b9ffb10d23b8a35f323807d4ef697f2d4f8
ahp002 <aliciap.ap6@gmail.com>
2024-12-01 23:26:16 -0800

    password checker done

--- password-checker/PasswordChecker.js
+++ password-checker/PasswordChecker.js
@@ -1,23 +1,22 @@
 var strength = {
-    0: "Bad ☹",
-    1: "Weak ☹",
-    2: "Meh ☹",
-    3: "Good ☺",
-    4: "Strong ☻"
+    0: "Bad",
+    1: "Weak",
+    2: "Meh",
+    3: "Good",
+    4: "Strong"
 };
 
 var password = document.getElementById('password');
 var meter = document.getElementById('password-strength-checker');
 var feedback = document.getElementById('password-strength-input');
+var strengthImage = document.getElementById('strength-image');
 
 password.addEventListener('input', function () {
     var val = password.value;
     var result = zxcvbn(val);
 
-    // Update the meter score
     meter.value = result.score;
 
-    // Update feedback text
     if (val !== "") {
         feedback.innerHTML =
             "Strength: <strong>" +
@@ -29,5 +28,36 @@ password.addEventListener('input', function () {
             "</span>";
     } else {
         feedback.innerHTML = "";
+        strengthImage.style.display = "none";
     }
+
+    var imageSrc = "";
+    switch (result.score) {
+        case 0:
+            imageSrc = "./images/bad.png";  
+            break;
+        case 1:
+            imageSrc = "./images/weak.png";  
+            break;
+        case 2:
+            imageSrc = "./images/meh.png";   
+            break;
+        case 3:
+            imageSrc = "./images/good.png";  
+            break;
+        case 4:
+            imageSrc = "./images/strong.png"; 
+            break;
+        default:
+            imageSrc = "";
+    }
+
+    if (val === "") {
+        strengthImage.style.display = "none";
+    }
+    else if (imageSrc) {
+        strengthImage.src = imageSrc;
+        strengthImage.style.display = "block"; 
+    }
+
 });
