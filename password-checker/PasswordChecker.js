e8872384e3565e46d9b140e3ac85d50345ea1b0e
ahp002 <aliciap.ap6@gmail.com>
2024-12-01 16:06:37 -0800

    everything

--- password-checker/PasswordChecker.js
+++ password-checker/PasswordChecker.js
@@ -4,23 +4,30 @@ var strength = {
     2: "Meh ☹",
     3: "Good ☺",
     4: "Strong ☻"
-}
+};
 
 var password = document.getElementById('password');
-var meter = document.getElementById('password-strength-checker');
-var text = document.getElementById('password-strength-input');
+var meter = document.getElementById('meter');
+var feedback = document.getElementById('password-strength-input');
 
-password.addEventListener('input', function()
-{
-var val = password.value;
-var result = zxcvbn(val);
+password.addEventListener('input', function () {
+    var val = password.value;
+    var result = zxcvbn(val);
 
-meter.value = result.score;
+    // Update the meter score
+    meter.value = result.score;
 
-if(val !== "") {
-    text.innerHTML = "Strength: " + "<strong>" + strength[result.score] + "</strong>" + "<span class='feedback'>" + result.feedback.warning + " " + result.feedback.suggestions + "</span"; 
-}
-else {
-    text.innerHTML = "";
-}
+    // Update feedback text
+    if (val !== "") {
+        feedback.innerHTML =
+            "Strength: <strong>" +
+            strength[result.score] +
+            "</strong><br><span class='feedback'>" +
+            result.feedback.warning +
+            " " +
+            result.feedback.suggestions +
+            "</span>";
+    } else {
+        feedback.innerHTML = "";
+    }
 });
